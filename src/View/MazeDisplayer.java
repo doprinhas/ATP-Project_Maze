08d17c05db04317d482f94593d65ffccc71a7e3f
dorpinhas <dorpinhas94@gmail.com>
2019-06-07 13:29:44 +0300

    jar file added

--- src/View/MazeDisplayer.java
+++ src/View/MazeDisplayer.java
@@ -3,4 +3,18 @@ package View;
 import javafx.scene.canvas.Canvas;
 
 public class MazeDisplayer extends Canvas {
+    
+    private int[][] maze;
+    private int characterRowPosition;
+    private int characterColPosition;
+    
+    public void setMaze(int[][] maze){
+        if (maze != null){
+            this.maze = maze;
+            redraw();
+        }
+    }
+
+    private void redraw() {
+    }
 }
